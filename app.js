6ab14e1d982c5c3c41995023038d3f7b14380151
kair <kair8m@gmail.com>
2022-04-25 23:16:37 +0300

    minor updates

--- app.js
+++ app.js
@@ -2,21 +2,36 @@ require('dotenv').config();
 const Discord = require('discord.js');
 
 const client = new Discord.Client({
-    intents: [
-        'GUILDS',
-        'GUILD_MESSAGES',
-    ],
+  intents: ['GUILDS', 'GUILD_MESSAGES', 'DIRECT_MESSAGES']
 });
 
 const {BOT_TOKEN} = process.env;
 
 if (!BOT_TOKEN) {
-    console.error('no token specified');
-    process.exit(-1);
+  console.error('no token specified');
+  process.exit(-1);
 }
 
-client.login(BOT_TOKEN);
+function setup() {
+  client.on('message', (message) => {
+    if (message.author.bot)
+      return;
+    const isAdmin = message.member.permissionsIn(message.channel).has('ADMINISTRATOR');
+    if (!isAdmin) {
+      client.channels(message.channel);
+      return;
+    }
 
-client.on('message', (msg) => {
-    console.log(msg);
-});
+    const user = client.channels.cache.get(message.channelId);
+    user.send('hi!');
+  });
+}
+
+client.login(BOT_TOKEN).then((r) => {
+  console.log('Successfully connected');
+  setup();
+})
+  .catch((res) => {
+    console.error(`Failed to login: ${res}`);
+    process.exit(-1);
+  });
